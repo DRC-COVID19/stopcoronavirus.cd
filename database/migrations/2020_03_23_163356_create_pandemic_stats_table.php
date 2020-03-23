c69fefc155dcdfc7da144976847c350b90089cb9
Junior Ngangeli <juniorngangeli3@gmail.com>
2020-03-23 17:49:20 +0100

    added model and migration for pandemic stat

---
+++ database/migrations/2020_03_23_163356_create_pandemic_stats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePandemicStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pandemic_stats', function (Blueprint $table) {
+            $table->id();
+            $table->string('label');
+            $table->integer('value')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pandemic_stats');
+    }
+}
