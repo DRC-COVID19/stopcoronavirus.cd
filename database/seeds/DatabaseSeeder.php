1f0113cf7eb8ee939164506620d4031c0b4bce53
Merveille KITOKO <merkitoko@gmail.com>
2020-03-24 13:58:14 +0100

    Put PermissionTableSeeder in databaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(PermissionTableSeeder::class);
         $this->call(AdminMenuTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(PostsTableSeeder::class);
