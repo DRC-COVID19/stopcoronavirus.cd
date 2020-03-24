30c2e7dfa620aff2731d643cc0e631fda50080e1
Merveille KITOKO <merkitoko@gmail.com>
2020-03-24 14:16:35 +0100

    admin users email nullable migration

--- database/migrations/2020_03_23_151427_email_admin_users.php
+++ database/migrations/2020_03_23_151427_email_admin_users.php
@@ -14,7 +14,7 @@ class EmailAdminUsers extends Migration
     public function up()
     {
         Schema::table('admin_users', function (Blueprint $table) {
-            $table->string('email');
+            $table->string('email')->nullable();
         });
     }
 
