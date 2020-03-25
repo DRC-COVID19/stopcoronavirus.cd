52f091c74b3f25f0ba556bb237b0059cf617b45d
Merveille KITOKO <merkitoko@gmail.com>
2020-03-25 12:42:04 +0100

    Redirect operator_sms, telco et validator_sms

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -7,11 +7,15 @@ use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Encore\Admin\Facades\Admin;
 
 class HomeController extends Controller
 {
     public function index(Content $content)
     {
+        if (!Admin::user()->isAdministrator() && ( Admin::user()->can('telco') || Admin::user()->can('validator_sms') || Admin::user()->can('operator_sms') )){
+            return redirect()->route('sms-diffusions.index');
+        }
         return $content
             ->title('Dashboard')
             ->description('Description...')
