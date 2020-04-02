b99da53f77c1f31a84ad8bc30142c4c6d4257db5
Nicolas Zanghi <me@nicolaszanghi.com>
2020-04-02 10:26:57 +0200

    add FRB on footer

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -118,10 +118,13 @@
             <div class="row align-items-end">
                 <div class="col-lg-8 col-md-12 col-lg-offset-2 col-md-offset-0 pb-2">
                     <p>
-                        avec l'appui d'Internews au travers du projet Médias2I de l'Union Européenne et du programme MSDA financé par l'USAID avec un financement complémentaire de la Coopération Suédoise
+                        {{ __("avec l'appui d'Internews au travers du projet Médias2I de l'Union Européenne et du programme MSDA financé par l'USAID avec un financement complémentaire de la Coopération Suédoise") }}
                     </p>
                     <p>
                         <img src="img/Logo_UE_MSDA_USAID.png" class="img-fluid" alt="Logo UE, USAID, Sverige, fhi360, Internews">
+                    </p>
+                    <p>
+                        {{ __('Un projet réalisé avec le soutien de la Fondation Roi Baudouin et de la Loterie Nationale') }}
                     </p>
                     <p class="pt-4">
                         <a href="https://www.kinshasadigital.com" target="_blank">Kinshasa Digital</a>
