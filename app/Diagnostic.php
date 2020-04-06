bb32bb9d9c86beb0b7781a9c724e119459092fd6
Merveille KITOKO <merkitoko@gmail.com>
2020-04-06 14:15:53 +0100

    Created diagnostics model and migration

---
+++ app/Diagnostic.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Diagnostic extends Model
+{
+    protected $fillable=[
+        'q-1',
+        'q-2',
+        'q-3',
+        'q-4',
+        'q-5',
+        'q-6',
+        'q-7',
+        'q-8',
+        'q-9',
+        'q-10',
+        'q-11',
+        'q-12',
+        'q-13',
+        'q-14',
+        'q-15',
+        'q-16',
+        'q-17',
+        'q-18',
+        'q-19',
+        'q-20',
+        'q-21',
+        'q-22',
+        'q-23',
+        'q-24',
+        'results_code',
+        'results_message',
+        'latitude',
+        'longitude',
+        
+    ];
+}
---
+++ database/migrations/2020_04_06_111110_create_diagnostics_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiagnosticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diagnostics', function (Blueprint $table) {
+            $table->id();
+            $table->integer('q-1');
+            $table->float('q-2')->nullable();
+            $table->integer('q-3');
+            $table->integer('q-4');
+            $table->integer('q-5');
+            $table->integer('q-6');
+            $table->integer('q-7');
+            $table->integer('q-8')->nullable();
+            $table->integer('q-9');
+            $table->integer('q-10');
+            $table->integer('q-11');
+            $table->integer('q-12');
+            $table->integer('q-13');
+            $table->integer('q-14');
+            $table->integer('q-15');
+            $table->integer('q-16');
+            $table->integer('q-17');
+            $table->integer('q-18');
+            $table->integer('q-19');
+            $table->integer('q-20');
+            $table->integer('q-21');
+            $table->integer('q-22');
+            $table->integer('q-23');
+            $table->string('q-24')->nullable();
+            $table->string('results_code');
+            $table->string('results_message');
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diagnostics');
+    }
+}
