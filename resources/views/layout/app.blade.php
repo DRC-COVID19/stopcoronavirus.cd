4e8e6c5839b6d336b1ef597436f099d427778d8a
Nicolas Zanghi <me@nicolaszanghi.com>
2020-04-13 14:16:20 +0200

    css version

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -33,7 +33,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700,900&display=swap" rel="stylesheet">
-    <link href="{{ asset('css/app.css?v=2') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v=3') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js?v=1') }}" defer></script>
 
     <!-- Google Tag Manager -->
