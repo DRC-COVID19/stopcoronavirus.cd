830fbc2e807b8de10990138f7ea067113298a4e0
Nicolas Zanghi <me@nicolaszanghi.com>
2020-04-13 10:12:38 +0200

    add socials on footer

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -159,6 +159,16 @@
                 </div>
             </div>
         </div>
+        <div class="footer-socials">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 p-2 text-right">
+                        <a href="https://twitter.com/aniciisrdc" target="_blank" class="mr-2"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.facebook.com/ANICiisRDC/" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
 </body>
 </html>
