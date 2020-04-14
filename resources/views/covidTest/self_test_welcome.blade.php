62e642b4a418017264a19852efce9fd9165382d4
Merveille KITOKO <merkitoko@gmail.com>
2020-04-14 13:30:35 +0100

    updated diagonstic welcome text

--- resources/views/covidTest/self_test_welcome.blade.php
+++ resources/views/covidTest/self_test_welcome.blade.php
@@ -5,10 +5,16 @@
     <div class="row">
         <div class="col-12 jumbotron">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Vous avez l'impression d'avoir des symptômes de Covid-19 et vous voulez savoir quoi faire si c'est le cas ? Ce questionnaire est là pour vous guider.
             </p>
             <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                Ce questionnaire est uniquement destiné à vous orienter en fonction de votre état de santé et des symptômes que vous signalez. Les conseils fournis n'ont aucune valeur médicale.
+            </p>
+            <p>
+                Vos réponses serviront à aider la recherche et la compréhension de l’épidémie de COVID-19.
+            </p>
+            <p>
+                Veuillez noter qu'aucune information vous identifiant, directement ou indirectement, n'est collectée.
             </p>
         </div>
     </div>
