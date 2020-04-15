575b157549b138bb29049f41c32116d2f43e3d33
Merveille KITOKO <merkitoko@gmail.com>
2020-04-15 13:28:37 +0100

    Created DiagnosticMenuSeeder

---
+++ database/seeds/DiagnosticMenu.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DiagnosticMenu extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admin_menu')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'parent_id' => 0,
+                'order' => 1,
+                'title' => 'Suivi tests diagnostiques',
+                'icon' => 'fa-bar-chart',
+                'uri' => 'diagnostics',
+                'permission' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+        ));
+        
+    }
+}
