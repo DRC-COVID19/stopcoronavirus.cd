80c952a5cb844c7d5ae67605807b884cc9cf9d37
Merveille KITOKO <merkitoko@gmail.com>
2020-04-16 14:25:39 +0100

    Fixed display mineur issues

--- app/Admin/Controllers/DiagnosticController.php
+++ app/Admin/Controllers/DiagnosticController.php
@@ -88,11 +88,11 @@ class DiagnosticController extends AdminController
 
         $grid->column('mineurs')->display(function () {
             $r = 0;
-            if ($this['q-1'] == 1 || $this['q-1'] == 2 || $this['q-1'] == 4) {
-                return $r++;
+            if ($this['q-1'] == 1 || $this['q-1'] == "2" || $this['q-1'] == 4) {
+                 $r++;
             }
             if ($this['q-7'] == 1) {
-                return $r++;
+                $r++;
             }
             return $r;
         })->filter('range');
