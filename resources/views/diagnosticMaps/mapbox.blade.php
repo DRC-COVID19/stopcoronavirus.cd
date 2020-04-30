1f83544a00eec52dcac5567c1eccb74526284115
Merveille KITOKO <merkitoko@gmail.com>
2020-04-30 18:01:08 +0100

    Css and js prod

--- resources/views/diagnosticMaps/mapbox.blade.php
+++ resources/views/diagnosticMaps/mapbox.blade.php
@@ -6,7 +6,7 @@
     <title>Document</title>
     <script src='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js'></script>
     <link href='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css' rel='stylesheet' />
-    <link href="{{ asset('css/app.css?v=4.9') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v=4.91') }}" rel="stylesheet">
 </head>
 <body>
     <div class="container-fluid">
@@ -30,6 +30,6 @@
         <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
         <p>{{__("Fetching Data")}}</p>
     </div>
-    <script src="{{ asset('js/app.js?v=1.8') }}" defer></script>
+    <script src="{{ asset('js/app.js?v=1.81') }}" defer></script>
 </body>
 </html>
