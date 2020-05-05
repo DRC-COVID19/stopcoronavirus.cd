25ab26832ad3e5544a7812747d2aea86c320fb0d
Merveille KITOKO <merkitoko@gmail.com>
2020-05-05 07:47:41 +0100

    app blade v 4.8

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -34,7 +34,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700,900&display=swap" rel="stylesheet">
     <link href="{{ asset('css/app.css?v=4.8') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js?v=1.7') }}" defer></script>
+    <script src="{{ asset('js/app.js?v=1.8') }}" defer></script>
 
     <!-- Google Tag Manager -->
     <script>
