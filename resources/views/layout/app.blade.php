613c6bbf145f1201d4b964d01eccc6d7aefdb060
Merveille KITOKO <merkitoko@gmail.com>
2020-05-29 09:13:00 +0100

    Whatsapp bot published

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -35,8 +35,8 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700,900&display=swap" rel="stylesheet">
-    <link href="{{ asset('css/app.css?v=4.8') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js?v=1.8') }}" defer></script>
+    <link href="{{ asset('css/app.css?v=4.9') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js?v=1.9') }}" defer></script>
 
     <!-- Google Tag Manager -->
     <script>
