30585fa4026fd35e0555e299ccbca2b4e06e2888
Merveille KITOKO <merkitoko@gmail.com>
2020-06-23 15:33:32 +0100

    Working[update site]

--- resources/views/covidTest/self_test_welcome.blade.php
+++ resources/views/covidTest/self_test_welcome.blade.php
@@ -15,8 +15,47 @@
                 Veuillez noter qu'aucune information vous identifiant, directement ou indirectement, n'est collectée.
             </p>
             <p class="text-center py-5">
-            <a href="{{route('selfTest.get')}}" class="btn btn-danger" href="">Démarrez le test</a>
+                <a href="{{route('selfTest.get')}}" class="btn btn-danger" href="">Démarrez le test</a>
             </p>
         </div>
+
+        {{-- <div class="row">
+            <div class="col-12">
+                <h2 class="mt-5">
+                    <i class="fas fa-virus"></i>
+                    {{ __("Quels sont les symptômes du COVID-19 ?") }}
+                </h2>
+            </div>
+        </div>
+        <div class="row my-4 home-symptome">
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-3 mb-md-5 ">
+                <div class="card">
+                    <div class="card-body"> {!! __("Fièvre, fatigue, toux sèche") !!}</div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-3 mb-md-5 ">
+                <div class="card">
+                    <div class="card-body"> {!! __("Pour certains patients: Congestion nasale, écoulement nasal, maux de gorge ou une diarrhée") !!}</div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-3 mb-md-5 ">
+                <div class="card">
+                    <div class="card-body">{!! __("Pour d’autres: aucun symptome") !!}</div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-3 mb-md-5 ">
+                <div class="card">
+                    <div class="card-body">{!! __("Environ 80 % des personnes guérissent sans avoir besoin de traitement particulier.") !!}</div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-3 mb-md-5 ">
+                <div class="card">
+                    <div class="card-body">
+                        {!! __("Les personnes âgées et celles qui ont d’autres problèmes de santé (hypertension artérielle, problèmes cardiaques ou diabète) ont plus de risques de présenter des symptômes graves.") !!}
+                    </div>
+                </div>
+            </div>
+        </div> --}}
+
     </div>
     @endsection
