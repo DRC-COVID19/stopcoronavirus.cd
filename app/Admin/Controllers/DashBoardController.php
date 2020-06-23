004dd09bf08f6e3836c639895c90dea876c2ddc1
Merveille KITOKO <merkitoko@gmail.com>
2020-06-23 17:42:29 +0100

    added redirect to route dashboard

--- app/Admin/Controllers/DashBoardController.php
+++ app/Admin/Controllers/DashBoardController.php
@@ -1,12 +1,10 @@
 <?php
 
 namespace App\Admin\Controllers;
-
 use App\Http\Controllers\Controller;
 
 
-class DashBoardController extends Controller
-{
+class DashBoardController extends Controller{
     public function index()
     {
         return view('diagnosticMaps.dashboard');
