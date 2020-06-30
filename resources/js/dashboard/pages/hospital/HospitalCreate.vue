2c3295dc216523822260f8d18e9f03298b2822eb
Merveille KITOKO <merkitoko@gmail.com>
2020-06-30 13:37:31 +0100

    working

--- resources/js/dashboard/pages/hospital/HospitalCreate.vue
+++ resources/js/dashboard/pages/hospital/HospitalCreate.vue
@@ -2,8 +2,17 @@
   <b-container>
     <b-row>
       <b-col>
-        <form-wizard shape="tab">
-          <tab-content title="Personal details">My first tab content</tab-content>
+        <form-wizard shape="tab" color="#2e5bff">
+          <tab-content title="Informationes  générales">
+            <b-row>
+              <b-col>
+                  <b-form-group>
+                      <label for="">Cas confirmé</label>
+                      <b-form-input type="text"/>
+                  </b-form-group>
+              </b-col>
+            </b-row>
+          </tab-content>
           <tab-content title="Additional Info">My second tab content</tab-content>
           <tab-content title="Last step">Yuhuuu! This seems pretty damn simple</tab-content>
         </form-wizard>
