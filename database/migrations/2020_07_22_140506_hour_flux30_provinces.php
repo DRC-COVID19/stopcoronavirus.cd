cbc85197abb1412a7818ac7b4b02bfad22d3e020
Merveille KITOKO <merkitoko@gmail.com>
2020-07-22 15:44:51 +0100

    added flux hour attribut

---
+++ database/migrations/2020_07_22_140506_hour_flux30_provinces.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class HourFlux30Provinces extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('flux30_provinces', function (Blueprint $table) {
+            $table->time('Hour')->nullable();
+            $table->string('Day_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('flux30_provinces', function (Blueprint $table) {
+            $table->dropColumn('Hour');
+            $table->dropColumn('Day_type');
+        });
+    }
+}
