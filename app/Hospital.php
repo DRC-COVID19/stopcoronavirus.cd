f1dfcd2eed67164233003c5f8b937ae8f6b31d9d
gedebamba <gedebamba3@gmail.com>
2020-07-23 05:41:48 +0100

    Historisation de la table hospital

--- app/Hospital.php
+++ app/Hospital.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Events\HospitalUpdatedEvent;
 
 class Hospital extends Model
 {
@@ -14,5 +15,18 @@ class Hospital extends Model
     public function agent()
     {
         return $this->belongsTo(Administrator::class,'agent_id');
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::updating(function($model){
+            $data = $model->getOriginal() ;
+            unset($data['created_at']);
+            unset($data['upfated_at']);
+
+            HospitalLog::create($data) ;
+        });
     }
 }
---
+++ app/HospitalLog.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HospitalLog extends Model
+{
+    //
+    protected $fillable = ['name', 'address', 'beds', 'respirators', 'masks', 'latitude',
+    'longitude', 'foam_beds', 'resuscitation_beds', 'doctors', 'nurses', 'para_medicals',
+    'agent_id'] ;
+
+    public function agent()
+    {
+        return $this->belongsTo(Administrator::class,'agent_id');
+    }
+}
