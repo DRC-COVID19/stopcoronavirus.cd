b19b77d1d536e039b2bc9a22126b3003a3dd374e
Merveille KITOKO <merkitoko@gmail.com>
2020-07-23 16:16:00 +0100

    created hospitalDataCreate

---
+++ resources/js/dashboard/pages/hospital/HospitalDataCreate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Header />
+    <b-container class="mt-4">
+      <Loading v-if="isLoading" class="h-100" />
+      <b-row v-else align-h="center">
+        <b-col cols="12" v-if="errors && errors.last_update">
+          <b-alert variant="danger" dismissible show>{{errors.last_update[0]}}</b-alert>
+        </b-col>
+        <b-col cols="12">
+          <b-link :to="{
+            name:'hospital.home'
+          }">
+            <span class="fa fa-chevron-left">Retour</span>
+          </b-link>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Header from "../../components/hospital/Header";
+import Loading from "../../components/Loading";
+export default {
+  components: {
+    Header,
+    Loading,
+  },
+};
+</script>
+
+<style>
+</style>
