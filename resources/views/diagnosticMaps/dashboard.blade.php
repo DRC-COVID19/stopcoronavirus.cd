bef8f39591f4bfcf4b7fbdcad4c0e522758bec84
Merveille KITOKO <merkitoko@gmail.com>
2020-07-23 14:07:35 +0100

    css and js prod

--- resources/views/diagnosticMaps/dashboard.blade.php
+++ resources/views/diagnosticMaps/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Dashboard COVID-19 RDC</title>
     <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css" rel="stylesheet" />
-    <link href="{{ mix('css/vueApp.css?v=2.822') }}" rel="stylesheet">
+    <link href="{{ asset('css/vueApp.css?v=2.8221') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -21,7 +21,7 @@
         </div>
     </div>
  
-    <script src="{{mix('js/vueApp.js?v=2.822') }}"></script>
+    <script src="{{asset('js/vueApp.js?v=2.8221') }}"></script>
 
 </body>
 </html>
