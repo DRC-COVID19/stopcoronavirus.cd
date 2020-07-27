ba44ec9f8efc3bfd00acf99293768d88108b07f6
Merveille KITOKO <merkitoko@gmail.com>
2020-07-27 16:10:07 +0100

    added role admin-hospita seeder

---
+++ database/seeds/role_admin_hospital.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class role_admin_hospital extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admin_roles')->insert([
+            [
+                "name" => "admin-hospital",
+                "slug" => 'admin-hospital',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+        ]);
+    }
+}
