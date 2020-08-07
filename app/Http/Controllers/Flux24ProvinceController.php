2980f9a3035d8a227d08149d752740f6cdbc93e7
Merveille KITOKO <merkitoko@gmail.com>
2020-08-07 17:22:32 +0100

    fixed namespace issues

--- app/Http/Controllers/Flux24ProvinceController.php
+++ app/Http/Controllers/Flux24ProvinceController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\FLux24Province;
+use App\Flux24Province;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
