cbab8547ef28664dcd055d22e9b470f75871af63
gedebamba <gedebamba3@gmail.com>
2020-08-16 11:24:41 +0100

    fix mix

--- resources/views/diagnosticMaps/dashboard.blade.php
+++ resources/views/diagnosticMaps/dashboard.blade.php
@@ -6,11 +6,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Dashboard COVID-19 RDC</title>
   <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css" rel="stylesheet" />
-<<<<<<< HEAD
   <link href="{{ asset('css/vueApp.css') }}" rel="stylesheet">
-=======
-  <link href="{{ mix('css/vueApp.css?v=2.841') }}" rel="stylesheet">
->>>>>>> dev
 </head>
 <body>
   <div id="app">
@@ -24,11 +20,7 @@
       <p>Fetching Data</p>
     </div>
 
-<<<<<<< HEAD
     <script src="{{asset('js/vueApp.js') }}"></script>
-=======
-    <script src="{{mix('js/vueApp.js?v=2.841') }}"></script>
->>>>>>> dev
 
 </body>
 </html>
