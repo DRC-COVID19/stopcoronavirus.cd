7bbc4907dd558174a908119cd8d82505576e8b4d
Merveille KITOKO <merkitoko@gmail.com>
2020-10-06 17:28:31 +0100

    update add township to hospital log migration

--- database/migrations/2020_08_31_164602_add_township_to_hospital_logs.php
+++ database/migrations/2020_08_31_164602_add_township_to_hospital_logs.php
@@ -6,32 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 class AddTownshipToHospitalLogs extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('hospital_logs', function (Blueprint $table) {
-            //
-            $table->unsignedBigInteger('township_id')->nullable() ;
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('hospital_logs', function (Blueprint $table) {
+      //
+      // $table->unsignedBigInteger('township_id')->nullable() ;
 
-            $table->foreign('township_id')->references('id')->on('townships');
-        });
-    }
+      // $table->foreign('township_id')->references('id')->on('townships');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('hospital_logs', function (Blueprint $table) {
-            //
-            $table->dropForeign('hospital_logs_township_id_foreign');
-            $table->dropColumn('township_id');
-        });
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('hospital_logs', function (Blueprint $table) {
+      //
+      $table->dropForeign('hospital_logs_township_id_foreign');
+      $table->dropColumn('township_id');
+    });
+  }
 }
