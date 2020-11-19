ec38689b54ff60789f57f087b5b6d48d2bd849a9
Merveille kitoko <merkitoko@gmail.com>
2020-11-19 16:42:26 +0100

    changed africell travel province date type

--- database/migrations/2020_11_19_151441_create_africel_travel_provinces_table.php
+++ database/migrations/2020_11_19_151441_create_africel_travel_provinces_table.php
@@ -16,7 +16,7 @@ class CreateAfricelTravelProvincesTable extends Migration
         Schema::create('africel_travel_provinces', function (Blueprint $table) {
             $table->id();
             $table->string('province_name');
-            $table->date('date');
+            $table->string('date');
             $table->double('volume');
             $table->timestamps();
         });
