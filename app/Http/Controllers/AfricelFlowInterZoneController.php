35be53c6a4dfa46acece74b8675dd86179eeae8a
Merveille kitoko <merkitoko@gmail.com>
2020-11-20 09:41:26 +0100

    created africel data controller

---
+++ app/Http/Controllers/AfricelFlowInterZoneController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AfricelFlowInterZone;
+use Illuminate\Http\Request;
+
+class AfricelFlowInterZoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AfricelFlowInterZone  $africelFlowInterZone
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AfricelFlowInterZone $africelFlowInterZone)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AfricelFlowInterZone  $africelFlowInterZone
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AfricelFlowInterZone $africelFlowInterZone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AfricelFlowInterZone  $africelFlowInterZone
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AfricelFlowInterZone $africelFlowInterZone)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AfricelFlowInterZone  $africelFlowInterZone
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AfricelFlowInterZone $africelFlowInterZone)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/AfricelPresenceZoneController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AfricelPresenceZone;
+use Illuminate\Http\Request;
+
+class AfricelPresenceZoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AfricelPresenceZone  $africelPresenceZone
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AfricelPresenceZone $africelPresenceZone)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AfricelPresenceZone  $africelPresenceZone
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AfricelPresenceZone $africelPresenceZone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AfricelPresenceZone  $africelPresenceZone
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AfricelPresenceZone $africelPresenceZone)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AfricelPresenceZone  $africelPresenceZone
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AfricelPresenceZone $africelPresenceZone)
+    {
+        //
+    }
+}
