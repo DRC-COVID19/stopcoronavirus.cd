4005fd5ffaae8631a0d82d50e71117d4f295753d
Merveille kitoko <merkitoko@gmail.com>
2021-02-08 16:42:06 +0100

    working administrator admin_users integration

--- resources/js/dashboard/routes.js
+++ resources/js/dashboard/routes.js
@@ -12,124 +12,141 @@ import HospitalAdmin from './pages/adminHospital/Home';
 import HospitalAdminData from './pages/adminHospital/HospitalData';
 import NotAcces from './pages/NotAccess';
 import Landing from './pages/Landing';
+import Administrator from './pages/Administrator';
+import AdminUserIndex from './pages/administrator/users/Index';
+import AdminUserCreate from './pages/administrator/users/Create';
 
 const adminDashboard = "admin-dashboard";
 const agentHospital = "agent-hospital";
 const adminHospital = "admin-hospital";
 
 export default [
-    {
-        name: "login",
-        path: "/login",
-        component: Login,
+  {
+    name: "login",
+    path: "/login",
+    component: Login,
 
+  },
+  {
+    name: "lostPassword",
+    path: "/lost-password",
+    component: LostPassword
+  },
+  {
+    name: "password.reset.thanks",
+    path: "/lost-password-send",
+    component: ResetPasswordThank
+  },
+  {
+    name: "password.reset.token",
+    path: "/password-reset/:token",
+    component: ResetPasseword
+  },
+  {
+    name: "password.reset.complete.thanks",
+    path: "/password-reset/complete",
+    component: PasswordResetComplete
+  },
+  {
+    path: "/",
+    component: Home,
+    name: "home",
+    meta: {
+      requiresAuth: true,
+      role: [adminDashboard]
     },
-    {
-        name: "lostPassword",
-        path: "/lost-password",
-        component: LostPassword
+  },
+  {
+    path: "/hospitals",
+    name: "hospital.home",
+    component: HospitalsHome,
+    meta: {
+      requiresAuth: true,
+      role: [agentHospital]
     },
-    {
-        name: "password.reset.thanks",
-        path: "/lost-password-send",
-        component: ResetPasswordThank
+  },
+  {
+    path: "/admin/hospitals",
+    name: "hospital.admin",
+    component: HospitalAdmin,
+    meta: {
+      requiresAuth: true,
+      role: [adminHospital]
     },
-    {
-        name: "password.reset.token",
-        path: "/password-reset/:token",
-        component: ResetPasseword
+  },
+  {
+    path: "/admin/hospitals/:hospital_id",
+    name: "hospital.admin.data",
+    component: HospitalAdminData,
+    meta: {
+      requiresAuth: true,
+      role: [adminHospital]
     },
-    {
-        name: "password.reset.complete.thanks",
-        path: "/password-reset/complete",
-        component: PasswordResetComplete
+  },
+  {
+    path: "/hospitals/data",
+    name: "hospital.data",
+    component: HospitalData,
+    meta: {
+      requiresAuth: true,
+      role: [agentHospital]
     },
-    {
-        path: "/",
-        component: Home,
-        name: "home",
-        meta: {
-            requiresAuth: true,
-            role: [adminDashboard]
-        },
+  },
+  {
+    path: "/hospitals/new",
+    name: "hospital.create",
+    component: HospitalsCreate,
+    meta: {
+      requiresAuth: true,
+      role: [agentHospital]
     },
-    {
-        path: "/hospitals",
-        name: "hospital.home",
-        component: HospitalsHome,
-        meta: {
-            requiresAuth: true,
-            role: [agentHospital]
-        },
+  },
+  {
+    path: "/hospitals/:hospital_id/show/:update_id",
+    name: "hospital.detail",
+    component: HospitalsDetail,
+    meta: {
+      requiresAuth: true,
+      role: [agentHospital, adminHospital]
     },
-    {
-        path: "/admin/hospitals",
-        name: "hospital.admin",
-        component: HospitalAdmin,
-        meta: {
-            requiresAuth: true,
-            role: [adminHospital]
-        },
+  },
+  {
+    path: "/hospitals/edit/:hospital_id",
+    name: "hospital.edit",
+    component: HospitalsCreate,
+    meta: {
+      requiresAuth: true,
+      role: [agentHospital]
     },
-    {
-        path: "/admin/hospitals/:hospital_id",
-        name: "hospital.admin.data",
-        component: HospitalAdminData,
-        meta: {
-            requiresAuth: true,
-            role: [adminHospital]
-        },
+  },
+  {
+    path: "/acces-denied",
+    name: "acces.denied",
+    component: NotAcces,
+    meta: {
+      requiresAuth: true,
     },
-    {
-        path: "/hospitals/data",
-        name: "hospital.data",
-        component: HospitalData,
-        meta: {
-            requiresAuth: true,
-            role: [agentHospital]
-        },
+  },
+  {
+    path: "/administrator",
+    component: Administrator,
+    children: [
+      {
+        path: 'users',
+        component: AdminUserIndex
+      },
+      {
+        path: 'users/create',
+        component: AdminUserCreate,
+      }
+    ]
+  },
+  {
+    path: "/landing",
+    name: "landing",
+    component: Landing,
+    meta: {
+      requiresAuth: true
     },
-    {
-        path: "/hospitals/new",
-        name: "hospital.create",
-        component: HospitalsCreate,
-        meta: {
-            requiresAuth: true,
-            role: [agentHospital]
-        },
-    },
-    {
-        path: "/hospitals/:hospital_id/show/:update_id",
-        name: "hospital.detail",
-        component: HospitalsDetail,
-        meta: {
-            requiresAuth: true,
-            role: [agentHospital,adminHospital]
-        },
-    },
-    {
-        path: "/hospitals/edit/:hospital_id",
-        name: "hospital.edit",
-        component: HospitalsCreate,
-        meta: {
-            requiresAuth: true,
-            role: [agentHospital]
-        },
-    },
-    {
-        path: "/acces-denied",
-        name: "acces.denied",
-        component: NotAcces,
-        meta: {
-            requiresAuth: true,
-        },
-    },
-    {
-        path: "/landing",
-        name: "landing",
-        component: Landing,
-        meta: {
-            requiresAuth: true
-        },
-    }
+  }
 ];
