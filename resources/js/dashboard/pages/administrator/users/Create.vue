1b0333f700a6c9b7abd0c96f98fc9be3dee66450
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-02-10 18:04:37 +0100

    Add the add user form

--- resources/js/dashboard/pages/administrator/users/Create.vue
+++ resources/js/dashboard/pages/administrator/users/Create.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="main">
     <Header :title="title" :iconClass="iconClass"/>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show" class="col-sm-10 col-md-6 mx-auto bg-light p-3" >
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show" class="col-sm-10 col-md-6 mx-auto my-3 bg-light p-3" >
+      <b-form-group
+        id="input-group-1"
+        label="Nom d'utilisateur:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.username"
+          type="text"
+          placeholder="Entrer le username"
+          required
+        ></b-form-input>
+      </b-form-group>
       <b-form-group
         id="input-group-1"
         label="Email address:"
         label-for="input-1"
-        description="We'll never share your email with anyone else."
       >
         <b-form-input
           id="input-1"
@@ -16,26 +28,35 @@
           required
         ></b-form-input>
       </b-form-group>
-
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+      <b-form-group id="input-group-2" label="Nom:" label-for="input-2">
         <b-form-input
           id="input-2"
           v-model="form.name"
-          placeholder="Enter name"
+          placeholder="Entrer le nom"
           required
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+      <b-form-group id="input-group-3" label="Role" label-for="input-3">
         <b-form-select
           id="input-3"
-          v-model="form.food"
-          :options="foods"
+          v-model="form.roles"
+          :options="roles"
           required
         ></b-form-select>
       </b-form-group>
+      <label for="text-password">Mot de passe</label>
+      <b-form-input type="password" id="text-password" aria-describedby="password-help-block" v-model="form.password"></b-form-input>
+      <b-form-text id="password-help-block">
+        Votre mot de passe doit contenir 8 carateres au minimum 
+      </b-form-text>
+      <label for="text-password-confirm">Confirmation de mot de passe</label>
+      <b-form-input type="password" id="text-password-confirm" aria-describedby="password-help-block" v-model="form.confirmPassword"></b-form-input>
+      <b-form-text id="password-help-block">
+        {{warningMissMatch}}
+      </b-form-text>
 
-      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+      <!--<b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
         <b-form-checkbox-group
           v-model="form.checked"
           id="checkboxes-4"
@@ -44,10 +65,9 @@
           <b-form-checkbox value="me">Check me out</b-form-checkbox>
           <b-form-checkbox value="that">Check that out</b-form-checkbox>
         </b-form-checkbox-group>
-      </b-form-group>
+      </b-form-group>-->
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+      <b-button type="submit" variant="primary" class="my-2 btn-dash-blue">Enreigistrer</b-button>
     </b-form>
     <!--<b-card class="mt-3" header="Form Data Result">
       <pre class="m-0">{{ form }}</pre>
@@ -66,12 +86,15 @@
         title: "Creation d'un utilisateur",
         iconClass: 'fas fa-plus-square',
         form: {
-          email: '',
+          username: '',
           name: '',
-          food: null,
+          roles: null,
+          email: '',
+          password: '',
+          confrimPassword: '',
           checked: []
         },
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        roles: [{ text: 'Choir un role', value: null }, 'Admininstrateur', 'Utilisateur'],
         show: true
       }
     },
@@ -92,6 +115,11 @@
         this.$nextTick(() => {
           this.show = true
         })
+      }
+    },
+    computed: {
+      warningMissMatch() {
+        return this.form.password === this.form.confrimPassword ? '' : 'Les mot de passes ne correspondent pas'
       }
     }
   }
