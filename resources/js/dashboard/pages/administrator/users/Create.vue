855dae73f8f13dc30e0bf81aa2f3a45fbe4a45c5
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-02-11 14:24:51 +0100

    Add validation feature to the form

--- resources/js/dashboard/pages/administrator/users/Create.vue
+++ resources/js/dashboard/pages/administrator/users/Create.vue
@@ -4,7 +4,7 @@
     <b-form @submit="onSubmit" @reset="onReset" v-if="show" class="col-sm-10 col-md-6 mx-auto my-3 bg-light p-3" >
       <b-form-group
         id="input-group-1"
-        label="Nom d'utilisateur:"
+        label="Nom d'utilisateur *"
         label-for="input-1"
       >
         <b-form-input
@@ -17,7 +17,7 @@
       </b-form-group>
       <b-form-group
         id="input-group-1"
-        label="Email address:"
+        label="Email address *"
         label-for="input-1"
       >
         <b-form-input
@@ -26,9 +26,11 @@
           type="email"
           placeholder="Enter email"
           required
+          @blur="validateMail()"
         ></b-form-input>
       </b-form-group>
-      <b-form-group id="input-group-2" label="Nom:" label-for="input-2">
+      <b-alert show v-if="validateMailMessage !== ''" variant="light">{{validateMailMessage}}</b-alert>
+      <b-form-group id="input-group-2" label="Nom *" label-for="input-2">
         <b-form-input
           id="input-2"
           v-model="form.name"
@@ -36,7 +38,7 @@
           required
         ></b-form-input>
       </b-form-group>
-      <label for="text-password">Roles</label>
+      <label for="text-password">Roles *</label>
       <b-form-checkbox-group
         id="checkbox-group-1"
         v-model="selected"
@@ -44,42 +46,25 @@
         :aria-describedby="ariaDescribedby"
         name="flavour-1"
       ></b-form-checkbox-group>
-      <label for="text-password">Choisir l'image de profil</label>
+      <!--<label for="text-password">Choisir l'image de profil</label>
       <b-form-file multiple>
         <template slot="file-name" slot-scope="{ names }">
           <b-badge variant="dark">{{ names[0] }}</b-badge>
           <b-badge v-if="names.length > 1" variant="dark" class="ml-1">
-            + {{ names.length - 1 }} More files
+            + {{ names.length - 1 }} Plus d'images
           </b-badge>
         </template>
-      </b-form-file>
-      <label for="text-password">Mot de passe</label>
+      </b-form-file>-->
+      <label for="text-password">Mot de passe *</label>
       <b-form-input type="password" id="text-password" aria-describedby="password-help-block" v-model="form.password"></b-form-input>
       <b-form-text id="password-help-block">
         Votre mot de passe doit contenir 8 carateres au minimum 
       </b-form-text>
-      <label for="text-password-confirm">Confirmation de mot de passe</label>
+      <label for="text-password-confirm">Confirmation de mot de passe *</label>
       <b-form-input type="password" id="text-password-confirm" aria-describedby="password-help-block" v-model="form.confirmPassword"></b-form-input>
-      <b-form-text id="password-help-block">
-        {{warningMissMatch}}
-      </b-form-text>
-
-      <!--<b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          v-model="form.checked"
-          id="checkboxes-4"
-          :aria-describedby="ariaDescribedby"
-        >
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>-->
-
+      <b-alert show variant="light">{{warningMissMatch}}</b-alert>
       <b-button type="submit" variant="primary" class="my-2 btn-dash-blue">Enreigistrer</b-button>
     </b-form>
-    <!--<b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>-->
   </div>
 </template>
 
@@ -93,6 +78,7 @@
       return {
         title: "Creation d'un utilisateur",
         iconClass: 'fas fa-plus-square',
+        validateMailMessage: '',
         form: {
           username: '',
           name: '',
@@ -115,14 +101,18 @@
         event.preventDefault()
         alert(JSON.stringify(this.form))
       },
+      validateMail () {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(String(this.form.email).toLowerCase())){
+          this.validateMailMessage = 'Adresse email incorrecte'
+        };
+      },
       onReset(event) {
         event.preventDefault()
-        // Reset our form values
         this.form.email = ''
         this.form.name = ''
         this.form.food = null
         this.form.checked = []
-        // Trick to reset/clear native browser form validation state
         this.show = false
         this.$nextTick(() => {
           this.show = true
@@ -130,9 +120,12 @@
       }
     },
     computed: {
-      warningMissMatch() {
+      warningMissMatch () {
         return this.form.password === this.form.confrimPassword ? '' : 'Les mot de passes ne correspondent pas'
-      }
+      },
+      // incorrectEmail () {
+      //   return this.validateMail() ? '' : 'Adresse email incorrecte'
+      // }
     }
   }
 </script>
