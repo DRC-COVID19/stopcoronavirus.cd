0bb1085b0f35450571af1f68910bcfa4fe2f8195
Merveille kitoko <merkitoko@gmail.com>
2021-02-22 17:20:59 +0100

    added change log component

---
+++ resources/js/dashboard/components/ChangeLogModal.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-modal :id="id" title="Change log">
+    <b-container>
+      <b-row>
+        <b-col cols="12" md="12">
+          <Timeline
+            :timeline-items="getChangeLogNotRead"
+            :message-when-no-items="messageWhenNoItems"
+            dateLocale="fr-fr"
+            class="timeline"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+    <template #modal-footer="{ hide }">
+      <b-link>Voir plus</b-link>
+      <b-button size="sm" variant="success" @click="hide(id)"> Fermer </b-button>
+    </template>
+  </b-modal>
+</template>
+
+<script>
+import { mapGetters, mapState } from "vuex";
+import Timeline from "timeline-vuejs";
+export default {
+  components: {
+    Timeline,
+  },
+  props: {
+    id: {
+      type: String,
+      default: "message-modal-id",
+    },
+  },
+  data() {
+    return {
+      messageWhenNoItems: "There are not items",
+    };
+  },
+  computed: {
+    ...mapGetters(["getChangeLogNotRead"]),
+  },
+};
+</script>
+
+<style>
+</style>
