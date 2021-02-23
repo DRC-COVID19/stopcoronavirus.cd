c840f68732afb6419f2c628d7e48b5f9c4a43358
Merveille kitoko <merkitoko@gmail.com>
2021-02-23 13:42:41 +0100

    added chang log contoller

---
+++ app/Http/Controllers/ChangeLogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ChangeLog;
+use Illuminate\Http\Request;
+
+class ChangeLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ChangeLog  $changeLog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ChangeLog $changeLog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ChangeLog  $changeLog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChangeLog $changeLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ChangeLog  $changeLog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ChangeLog $changeLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ChangeLog  $changeLog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChangeLog $changeLog)
+    {
+        //
+    }
+}
