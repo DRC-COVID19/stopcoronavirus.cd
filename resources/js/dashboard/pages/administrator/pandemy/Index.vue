c9cad04ffc4de7da3f1d5a5a4357dbf4feafe681
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-10 11:29:36 +0100

    Update the pandemics page with table and completed form

--- resources/js/dashboard/pages/administrator/pandemy/Index.vue
+++ resources/js/dashboard/pages/administrator/pandemy/Index.vue
@@ -20,6 +20,14 @@
                   :max="new Date()"
                   required
                 />
+                <label class="mt-2 text-dash-color" for="check-group-1">Zone de sante *</label>
+                <v-select
+                  class="input-dash"
+                  v-model="form.zone"
+                  :options="zones"
+                  label="name"
+                  :reduce="(item) => item.id"
+                />
               </b-form-group>
             </b-row>
             <b-row class="d-flex justify-content-start">
@@ -104,12 +112,65 @@
                 </b-form-group>
               </b-col>
             </b-row>
+            <b-row>
+              <b-button
+                type="submit"
+                class="btn-dash-sucess"
+                :disabled="isCreating"
+              >
+                <span v-if="isCreating"
+                  ><b-spinner class="align-middle"></b-spinner>
+                  <span>en cours ...</span>
+                </span>
+                <div v-else>
+                  <span v-if="isUpdating">Modifier</span>
+                  <span v-else>Enregistrer</span>
+                </div>
+              </b-button>
+              <b-button
+                :disabled="isCreating"
+                v-if="isUpdating"
+                class="btn-dash-danger"
+                @click="cancelEditMode"
+                >Annuler</b-button
+              >
+            </b-row>
           </b-form>
         </b-card>
       </b-col>
       <b-col md="8">
-        <b-table>
-          
+        <Header title="Pandemie" iconClass="fas fa-procedures" />
+        <b-row class="my-3" align-h="start">
+          <b-col cols="12" md="6">
+            <b-form-datepicker
+              label-today-button="Aujourd'hui"
+              label-reset-button="Effacer"
+              reset-button
+              today-button
+              :max="new Date()"
+              v-model="filter"
+              placeholder="Choisir la date"
+              class="mb-2"
+            >
+            </b-form-datepicker>
+          </b-col>
+        </b-row>
+        <b-skeleton-table
+          v-if="isLoading"
+          :rows="15"
+          :columns="5"
+          :table-props="{ bordered: false, striped: true, responsive: true }"
+        >
+        </b-skeleton-table>
+        <b-table
+          v-else
+          hover
+          striped
+          responsive
+          :items="listPandemics"
+          :fields="fields"
+        >
+
         </b-table>
       </b-col>
     </b-row>
@@ -124,12 +185,49 @@
     },
     data () {
       return {
+        fields: [
+          {
+            key: "id",
+            label: "ID",
+          },
+          {
+            key: "confirmed",
+            label: "Confirmes",
+          },
+          {
+            key: "sick",
+            lablel: "Actifs",
+          },
+          {
+            key: "healed",
+            lablel: "Gueris",
+          },
+          {
+            key: "dead",
+            label: "Deces",
+          },
+          {
+            key: "health_zone",
+            label: "Zone de sante",
+          },
+        ],
         errors: {},
+        filter: null,
+        zones: [
+          "Nord-Kivu", "Kinshasa", "Sud-Kivu", "Katanga"
+        ],
         form: {},
         isUpdating: false,
         currentItem: {},
         isCreating: false,
-        isLoading: false
+        isLoading: false,
+        listPandemics: [
+          {id: '1', confirmed: 12, sick: 13, healed: 23, dead: 34, health_zone: 'Afia'},
+          {id: '1', confirmed: 12, sick: 13, healed: 23, dead: 34, health_zone: 'Afia'},
+          {id: '1', confirmed: 12, sick: 13, healed: 23, dead: 34, health_zone: 'Afia'},
+          {id: '1', confirmed: 12, sick: 13, healed: 23, dead: 34, health_zone: 'Afia'},
+          {id: '1', confirmed: 12, sick: 13, healed: 23, dead: 34, health_zone: 'Afia'}
+        ]
       }
     }
   }
