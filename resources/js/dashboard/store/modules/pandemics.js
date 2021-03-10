43494bb96ace630ef9acf2795602e9cb8bacab7d
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-10 09:44:13 +0100

    Add the pandemics page model

--- resources/js/dashboard/store/modules/pandemics.js
+++ resources/js/dashboard/store/modules/pandemics.js
@@ -2,16 +2,26 @@ export default {
   state: {
     listPandemics: {},
     currentPage: 1,
-    selectedChangeLog: null,
+    selectedPandemics: null,
     isLoading: false,
     isUpdating: false,
     isDeleted: false
   },
   mutations: {
-    
+    setListPandemics (state, payload) {
+      state.listPandemics = payload;
+    },
+    setSelectedPandemics (state, payload) {
+      state.selectedPandemics = payload;
+    }
   },
   actions: {
+    getListPandemics () {
 
+    },
+    searchPandemics () {
+      
+    }
   },
 
 }
