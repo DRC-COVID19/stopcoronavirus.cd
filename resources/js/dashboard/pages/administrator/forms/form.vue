01ecc6c1c07e6a9b19d78e092a18b04327149388
gedeon-E <gedebamba3@gmail.com>
2021-09-01 17:23:03 +0100

    set grid of components on forms page

--- resources/js/dashboard/pages/administrator/forms/form.vue
+++ resources/js/dashboard/pages/administrator/forms/form.vue
@@ -1,46 +1,55 @@
 <template>
   <b-container>
-      <b-row  class="mb-3">
-        <b-col cols="12" md="6" lg="4" class="mt-4">
-          <b-card>
-            <b-list-group flush>
-              <b-list-group-item>
-                <small class="text-muted"> Nom du formulaire </small> <br>
-                <span> {{form.title}} </span>
-              </b-list-group-item>
-              <b-list-group-item>
-                <small class="text-muted"> Réccurence </small> <br>
-                <span> {{form.form_recurrence.name}} </span>
-              </b-list-group-item>
-              <b-list-group-item
-                v-if="form.form_recurrence_value"
-              >
-                <small class="text-muted"> Valeur de la recurrence </small> <br>
-                <span> {{form.form_recurrence_value}} </span>
-              </b-list-group-item>
-              <b-list-group-item>
-                <small class="text-muted"> Statut de publication </small> <br>
-                <span> {{ form.publish ? 'Publié' : 'Non publié' }} </span>
-              </b-list-group-item>
-              <b-list-group-item>
-                <small class="text-muted"> Date de création </small> <br>
-                <span> {{ formatDateFns(form.created_at) }} </span>
-              </b-list-group-item>
-            </b-list-group>
-            <b-card-footer>
-              <a href="#" class="card-link d-flex justify-content-between align-items-center">
-                Aperçu
-                <i class="fas fa-arrow-right" aria-hidden="true"></i>
-              </a>
-            </b-card-footer>
-          </b-card>
+      <b-row  class="mb-3 mt-4">
+        <b-col cols="12" md="5" lg="4">
+          <b-col cols="12" class="p-0">
+            <b-card>
+              <b-list-group flush>
+                <b-list-group-item>
+                  <small class="text-muted"> Nom du formulaire </small> <br>
+                  <span> {{form.title}} </span>
+                </b-list-group-item>
+                <b-list-group-item>
+                  <small class="text-muted"> Réccurence </small> <br>
+                  <span> {{form.form_recurrence.name}} </span>
+                </b-list-group-item>
+                <b-list-group-item
+                  v-if="form.form_recurrence_value"
+                >
+                  <small class="text-muted"> Valeur de la recurrence </small> <br>
+                  <span> {{form.form_recurrence_value}} </span>
+                </b-list-group-item>
+                <b-list-group-item>
+                  <small class="text-muted"> Statut de publication </small> <br>
+                  <span> {{ form.publish ? 'Publié' : 'Non publié' }} </span>
+                </b-list-group-item>
+                <b-list-group-item>
+                  <small class="text-muted"> Date de création </small> <br>
+                  <span> {{ formatDateFns(form.created_at) }} </span>
+                </b-list-group-item>
+              </b-list-group>
+              <b-card-footer>
+                <a href="#" class="card-link d-flex justify-content-between align-items-center">
+                  Aperçu
+                  <i class="fas fa-arrow-right" aria-hidden="true"></i>
+                </a>
+              </b-card-footer>
+            </b-card>
+          </b-col>
+        </b-col>
+        <b-col cols="12" md="7" lg="8">
+          <FormFieldIndex />
         </b-col>
       </b-row>
   </b-container>
 </template>
 <script>
 import { mapActions } from 'vuex'
+import FormFieldIndex from './components/formField/index.vue'
 export default {
+  components: {
+    FormFieldIndex
+  },
   data(){
     return{
       form:{}
