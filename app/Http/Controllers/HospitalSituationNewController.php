fcb10330ad0cf8a149866e5bf54cb75af24cb869
rkota <rosaire@kinshasadigital.com>
2022-01-23 15:05:06 +0100

    refactor: update getSituationsByHospital  method in HospitalSituationNewController

--- app/Http/Controllers/HospitalSituationNewController.php
+++ app/Http/Controllers/HospitalSituationNewController.php
@@ -124,100 +124,32 @@ class HospitalSituationNewController extends Controller
             ]
         ])->validate();
     }
-    public function getSituationHospitalsAll()
+    
+       /**
+     * Store a newly created resource in storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+  public function getSituationHospitalsAll()
     {
-        // $hospitalSituation = DB::table('hospital_situations_new')
-        // ->join('form_fields', 'hospital_situations_new.form_field_id', '=', 'form_fields.id')
-        // ->join('form_steps', 'form_fields.form_step_id', '=', 'form_steps.id')
-        // ->where('form_fields.name', '=', 'Nombre des respirateurs réservés pour des cas COVID-19')
-        // ->orWhere('form_fields.name', '=', 'Nombre des lits de réanimation occupés par des cas COVID-19')
-        // ->where('form_fields.name', '<>', 'EPI en manque')
-        // ->where('form_fields.name', '<>', 'Nom du CTCO de référence')
-        // ->select(
-        //      'form_fields.name as form_field_name',
-        //     DB::raw('SUM(CAST(hospital_situations_new.value as INT)) as form_field_value'),
-        //     'form_fields.capacity as form_field_capacity',
-        //     'form_fields.form_step_id as form_step_id',
-        //     'form_steps.title as form_step_title'
-        // )
-        // ->groupBy('form_step_id','form_step_title', 'form_field_name', 'form_field_capacity')
-        // ->get();
-          
-        // return $hospitalSituation;
-           $lastUpdate='2022-01-14';
-            $observation_start='2022-01-10';
-            $observation_end='2022-01-18';
-            $hospital='2';
-            $township='21';
             try{
-                // On réccupère toutes les dates où une mise à jour a pu etre poster
-                // Surtout utile pour l'evolution globale
-    
-                 $lastUpdates = HospitalSituationNew::where(function ($query) use ($hospital, $township) {
-                    if ($hospital){
-                        $query->where('hospital_id', intval($hospital));
-                    }
-                    else if($township){
-                        $query->whereRaw('(SELECT township_id FROM hospitals WHERE id = hospital_id) = ?' ,
-                        [$township]) ;
-                    }
-                    })
-                ->whereRaw('DATE(last_update) BETWEEN ? AND ? ', [$observation_start, $observation_end])
-                ->select('last_update')
-                ->pluck('last_update')
-                ->unique()->sort()->values() ;
-    
-                $results = [
-                    'last_update' => [],
-                    'form_fields_names' => [],
-                  ];
-                  foreach ($lastUpdates as  $lastUpdate) {
-                      
-                $hospitalSituation = HospitalSituationNew::where(function ($query) use ($hospital, $township) {
-                        if ($hospital){
-                            $query->where('hospital_id', intval($hospital));
-                        }else if($township){
-                          $query->whereRaw('(SELECT township_id FROM hospitals WHERE id = hospital_id) = ?' ,
-                          [$township]) ;
-                        }
-                    })
+                $hospitalSituation = DB::table('hospital_situations_new')
                 ->join('form_fields', 'hospital_situations_new.form_field_id', '=', 'form_fields.id')
                 ->join('form_steps', 'form_fields.form_step_id', '=', 'form_steps.id')
-                ->where('form_fields.name', '=', 'Nombre des respirateurs réservés pour des cas COVID-19')
-                ->orWhere('form_fields.name', '=', 'Nombre des lits de réanimation occupés par des cas COVID-19')
                 ->where('form_fields.name', '<>', 'EPI en manque')
                 ->where('form_fields.name', '<>', 'Nom du CTCO de référence')
                 ->select(
                      'form_fields.name as form_field_name',
-                    DB::raw('sum(CAST(hospital_situations_new.value as INT)) as form_field_value'),
+                    DB::raw('SUM(CAST(hospital_situations_new.value as INT)) as form_field_value'),
                     'form_fields.capacity as form_field_capacity',
                     'form_fields.form_step_id as form_step_id',
                     'form_steps.title as form_step_title'
                 )
-                ->where('last_update' , '<=' , $lastUpdate)
-                ->whereRaw('DATE(last_update) BETWEEN ? AND ?', [$observation_start, $observation_end])
-                ->whereNotExists(function($query) use($lastUpdate){
-                    // C'est ici qu'on s'assure que la situation actuellemnent lu est la dernière connu
-                    // pour l'hopital x à la date $last_update sur laquelle on boucle
-                    $query->select(DB::raw(1))
-                    ->from(DB::raw('hospital_situations_new AS h'))
-                    ->whereRaw("h.hospital_id = hospital_situations_new.hospital_id
-                        AND h.last_update <='{$lastUpdate}'
-                        AND (
-                          h.last_update > hospital_situations_new.last_update OR
-                          (h.last_update = hospital_situations_new.last_update AND h.id > hospital_situations_new.id )
-                        )
-                      ") ;
-                })
                 ->groupBy('form_step_id','form_step_title', 'form_field_name', 'form_field_capacity')
                 ->get();
                   
-               
-                $results['last_update'][] = $lastUpdate ;
-                $results['form_fields_names'][] = $hospitalSituation;
-            }
     
-              return response()->json($results,200,[],JSON_NUMERIC_CHECK);
+              return response()->json($hospitalSituation,200,[],JSON_NUMERIC_CHECK);
           } catch (\Throwable $th) {
               if (env('APP_DEBUG') == true) {
                   return response($th)->setStatusCode(500);
@@ -226,6 +158,51 @@ class HospitalSituationNewController extends Controller
           }
     }
 
+       /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function getSituationsByHospital(Request $request)
+    {
+        try{
+
+        $hospitalSituation = DB::table('hospital_situations_new')
+        ->join('form_fields', 'hospital_situations_new.form_field_id', '=', 'form_fields.id')
+        ->join('form_steps', 'form_fields.form_step_id', '=', 'form_steps.id')
+        ->join('hospitals', 'hospital_situations_new.hospital_id', '=', 'hospitals.id')
+        ->where('hospital_situations_new.hospital_id', '=', $request->query('hospital_id'))
+        ->where('form_fields.name', '<>', 'EPI en manque')
+        ->where('form_fields.name', '<>', 'Nom du CTCO de référence')
+        ->select(
+             'form_fields.name as form_field_name',
+            DB::raw('SUM(CAST(hospital_situations_new.value as INT)) as form_field_value'),
+            'form_fields.capacity as form_field_capacity',
+            'form_fields.form_step_id as form_step_id',
+            'form_steps.title as form_step_title'
+        )
+        ->groupBy('form_step_id','form_step_title', 'form_field_name', 'form_field_capacity')
+        ->get();
+
+
+        return response()->json($hospitalSituation,200,[],JSON_NUMERIC_CHECK);
+    } catch (\Throwable $th) {
+        if (env('APP_DEBUG') == true) {
+            return response($th)->setStatusCode(500);
+        }
+        return response($th->getMessage())->setStatusCode(500);
+    }
+      
+    }
+
+       /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function getSituations($hospital = null, $request)
     {
            
@@ -313,24 +290,8 @@ class HospitalSituationNewController extends Controller
       }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function getSituationByHospitals(Request $request)
-    {
-
-        $hospitalSituation = DB::table('hospital_situations_new')
-            ->join('form_fields', 'hospital_situations_new.form_field_id', '=', 'form_fields.id')
-            ->join('hospitals', 'hospital_situations_new.hospital_id', '=', 'hospitals.id')
-            ->where('hospital_situations_new.hospital_id', '=', $request->hospital_id)
-            ->select('hospitals.*', 'form_fields.name', 'hospital_situations_new.value')
-            ->get();
-        return $hospitalSituation;
-    }
-
+ 
+   
     /**
      * Get the guard to be used during authentication.
      *
