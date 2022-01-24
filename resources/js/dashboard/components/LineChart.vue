f043a7e5ed0fb8d2054de55847129bead0a56965
emmie125 <mulangaemmanuella@gmail.com>
2022-01-24 10:03:20 +0100

    feat: add a linechart

---
+++ resources/js/dashboard/components/LineChart.vue
@@ -0,0 +1,35 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  data() {
+    return {
+      options: { responsive: true },
+      chartdata:{ 
+         labels: [
+          "Galiema",
+          "CUK",
+          "Diamant",
+          "MONKOLE",
+          "SAINT JOSEPH",
+          "VIJANA",
+          "KINKOLE"
+        ],
+        datasets: [
+          {
+            label: "Data 1",
+            data: [2, 10, 5, 9, 0, 6, 20],
+            backgroundColor: "transparent",
+            borderColor: "rgba(1, 116, 188, 0.50)",
+            pointBackgroundColor: "rgba(171, 71, 188, 1)"
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  },
+};
+</script>
