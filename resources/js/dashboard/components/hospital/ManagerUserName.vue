ab19008f93e132cd52fe93595f8e2f575c2e0e84
emmie125 <mulangaemmanuella@gmail.com>
2022-03-09 12:04:07 +0100

    feat: Add input lastName in the situation hospital

--- resources/js/dashboard/components/hospital/ManagerUserName.vue
+++ resources/js/dashboard/components/hospital/ManagerUserName.vue
@@ -5,8 +5,12 @@
         <b-col>
           <b-form @submit.prevent="hospitalManagerNameSubmit">
             <b-form-group>
-              <label class="text-dash-color">Veuillez entrer votre nom complet pour continuer</label>
-              <b-form-input v-model="localhospitalManagerName" required class="input-dash" />
+              <label class="text-dash-color">Veuillez entrer votre nom pour continuer</label>
+              <b-form-input v-model="identity.name" required class="input-dash" />
+            </b-form-group>
+            <b-form-group>
+              <label class="text-dash-color">Veuillez entrer votre prénom complet pour continuer</label>
+              <b-form-input  v-model="identity.lastName" required class="input-dash" />
             </b-form-group>
             <b-button type="submit" class="btn-dash-blue">Continuer</b-button>
           </b-form>
@@ -17,29 +21,41 @@
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import { mapMutations } from 'vuex'
 export default {
   props: {
     id: {
       type: String,
-      default: "nameModal"
+      default: 'nameModal'
     }
   },
-  data() {
+  data () {
     return {
-      localhospitalManagerName: null
-    };
+      identity: {
+        name: null,
+        lastName: null
+      }
+    }
+  },
+  computed: {
+    localhospitalManagerName () {
+      if (this.identity.name && this.identity.lastName) {
+        return this.identity.name + ' ' + this.identity.lastName
+      } else {
+        return null
+      }
+    }
   },
   methods: {
-    ...mapMutations(["setHospitalManagerName"]),
-    hospitalManagerNameSubmit() {
+    ...mapMutations(['setHospitalManagerName']),
+    hospitalManagerNameSubmit () {
       if (this.localhospitalManagerName) {
-        this.setHospitalManagerName(this.localhospitalManagerName);
-        this.$bvModal.hide(this.id);
+        this.setHospitalManagerName(this.localhospitalManagerName)
+        this.$bvModal.hide(this.id)
       }
     }
   }
-};
+}
 </script>
 
 <style>
