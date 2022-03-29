79b817343b809ec7db6ec6d0babf26bd4c3de4e3
rkota <rosaire@kinshasadigital.com>
2022-03-29 10:46:59 +0100

    refactor: update users(Create) page

--- resources/js/dashboard/pages/administrator/users/components/Create.vue
+++ resources/js/dashboard/pages/administrator/users/components/Create.vue
@@ -83,7 +83,7 @@
         v-model="form.roles"
         multiple
         :options="roles"
-        label="name"
+        label="label"
         :reduce="(item) => item.id"
         :searchable ="false"
       />
