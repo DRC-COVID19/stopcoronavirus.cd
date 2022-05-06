fbb8944d91b923b550b575bea9e10952cd9c82a0
rkota <rosaire@kinshasadigital.com>
2022-05-06 10:43:19 +0100

    feat: require listFormCard in ListForms

--- resources/js/dashboard/pages/administrator/forms2/components/listForm/ListForms.vue
+++ resources/js/dashboard/pages/administrator/forms2/components/listForm/ListForms.vue
@@ -7,18 +7,7 @@
                        v-for="(form, index) in formsList" :key="index"
                      md="3" sm="12"
                      >
-                         <div class="card-list">
-                             <div class="card-list-header">
-                                 <h4>{{ form.title}}</h4>
-                             </div>
-                             <div class="card-list-body px-2 pt-3">
-                                 <div class="d-flex justify-content-between">
-                                     <h6>Crée  le {{ moment(form.created_at).format('DD/MM/Y')}}</h6>
-                                     <h6>{{ form.form_recurrence.name}}</h6>
-                                 </div>
-                                 <h6 class="text-danger">5 Réponses</h6>
-                             </div>
-                         </div>
+                         <list-form-card :form="form"/>
                      </b-col>
                      </b-row>
                  </b-container>
@@ -27,9 +16,11 @@
 </template>
 
 <script>
+import ListFormCard from './ListFormCard.vue'
 export default {
   props: {
-    formsList: {
+    formsLis
+    ListFormCardt: {
       type: Array,
       default: () => ([]),
       required: false
