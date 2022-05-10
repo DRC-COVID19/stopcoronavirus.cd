d59b64733a938420ce4ca745ac310b8f25678c27
emmie125 <mulangaemmanuella@gmail.com>
2022-05-10 11:27:18 +0200

    feat: add a  feature step

---
+++ resources/js/dashboard/pages/administrator/forms2/formDetail/components/formStep/FormStepList.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-row class="">
+    <b-row class="">
+      <b-col class=""
+        ><div>Étape</div>
+        <div class="d-lg-none">
+          <b-button class="btn-dash-blue d-block">
+            <i class="fas fa-plus responsive-label"></i>
+          </b-button>
+        </div>
+      </b-col>
+    </b-row>
+    <b-row></b-row>
+  </b-row>
+</template>
+<script>
+export default {};
+</script>
--- resources/js/dashboard/pages/administrator/forms2/formDetail/formCreation.vue
+++ resources/js/dashboard/pages/administrator/forms2/formDetail/formCreation.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>Création</div>
+  <b-container
+    class="h-100 mx-0 my-0 px-0 justify-content-start container-creation"
+  >
+    <b-row class="h-100">
+      <b-col lg="3" class="bg-white container-creation-list">
+        <FormStepIndex />
+      </b-col>
+      <b-col lg="9" class="w-100">
+       
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
+<script>
+import FormStepIndex from "./components/formStep/FormStepIndex.vue";
+export default {
+  components: {
+    FormStepIndex,
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@~/sass/_variables";
+.container-creation-list {
+  padding-top: 8% !important;
+}
+</style>
