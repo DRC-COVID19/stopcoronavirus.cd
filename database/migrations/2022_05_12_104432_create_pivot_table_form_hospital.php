712692777ca9ed8b6143b0e599270b26473e6558
rkota <rosaire@kinshasadigital.com>
2022-05-12 12:06:58 +0100

    feat: add migration of pivot table between forms and hospitals tables

---
+++ database/migrations/2022_05_12_104432_create_pivot_table_form_hospital.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePivotTableFormHospital extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_hospital', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('form_id')->constrained();
+            $table->foreignId('hospital_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('form_hospital', function (Blueprint $table) {
+            //
+        });
+    }
+}
