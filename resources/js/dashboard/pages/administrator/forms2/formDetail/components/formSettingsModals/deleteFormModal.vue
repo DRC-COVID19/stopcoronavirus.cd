f3da6cfd5aee35a71fb6ecccdb8ebbbd1d041c13
rkota <rosaire@kinshasadigital.com>
2022-05-13 12:23:00 +0100

    feat: add deleteFormModal component

--- resources/js/dashboard/pages/administrator/forms2/formDetail/components/formSettingsModals/deleteFormModal.vue
+++ resources/js/dashboard/pages/administrator/forms2/formDetail/components/formSettingsModals/deleteFormModal.vue
@@ -1,12 +1,32 @@
 <template>
-  <b-modal id="deleteForm" centered>
-      <h4>deleted</h4>
+  <b-modal id="deleteForm" centered title="Suppression du formulaire" hide-header-close hide-footer>
+      <p>Voulez-vous vraiment supprimer ce formulaire ?</p>
+              <div class="mt-4 text-center">
+                <b-button
+                @click="onCancelDeleteForm"
+                type="submit"
+                variant="outline-danger"
+                class="mr-3"
+                >Annuler</b-button
+                >
+                 <b-button
+                @click="onDeleteForm"
+                type="submit"
+                variant="primary"
+                >Supprimer</b-button
+                >
+              </div>
+            </b-form>
   </b-modal>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    onCancelDeleteForm () {
+      this.$bvModal.hide('deleteForm')
+    }
+  }
 }
 </script>
 
