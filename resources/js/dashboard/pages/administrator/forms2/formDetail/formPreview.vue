be471111248916f0d56a6bc9b9c9c4549b944c2a
Gédéon Ebamba <gedebamba3@gmail.com>
2022-05-17 15:50:04 +0100

    feat: preview form

--- resources/js/dashboard/pages/administrator/forms2/formDetail/formPreview.vue
+++ resources/js/dashboard/pages/administrator/forms2/formDetail/formPreview.vue
@@ -2,16 +2,21 @@
   <b-container class="mx-0 px-0 d-flex justify-content-center" fluid>
     <b-row class="w-100 h-100 mx-0 px-0">
       <b-col lg="12" class="w-100 mx-0 px-0">
-        <HospitalCard />
+        <FormView
+          back-route-name="administrator.forms.show.creation"
+          preview
+        />
       </b-col>
     </b-row>
   </b-container>
 </template>
 <script>
-import HospitalCard from "../../../../components/hospitalCard/HospitalCard.vue";
+import FormView from '../../../../components/forms/FormView.vue'
 export default {
   components: {
-    HospitalCard,
-  },
-};
+    FormView
+  }
+}
 </script>
+
+<style lang="scss" scoped>
--- resources/js/dashboard/pages/hospital/HospitalForm.vue
+++ resources/js/dashboard/pages/hospital/HospitalForm.vue
@@ -12,9 +12,6 @@
           <b-link :to="backRoute">
             <span class="fa fa-chevron-left">Retour</span>
           </b-link>
-          <!-- <b-col cols="12" v-if="errors && errors.last_update">
-                    <b-alert variant="danger" dismissible show>Vous ne pouvez plus soumettre pour le  {{ moment(completedForm.last_update).format("DD/MM/Y") }}! car, les données ont déjà été soumis.</b-alert>
-                  </b-col> -->
           <h3 v-if="isUpdateMode" class="mb-4 mt-4">
             Modifier la mise à jour du
             {{ moment(completedForm.last_update).format("DD/MM/Y") }}
@@ -57,9 +54,7 @@
                       <b-row>
                         <b-col class="col-sm-12 col-md-12">
                           <FormFieldInput
-                            v-model="
-                              completedForm.completed_form_fields[formField.id]
-                            "
+                            v-model=" completedForm.completed_form_fields[formField.id]"
                             :type="formField.form_field_type.name"
                             :placeholder="`Entrer ${formField.name}`"
                             :id="formField.name"
@@ -87,9 +82,7 @@
                         <ul>
                           <li>
                             {{ formField.name }} :
-                            {{
-                              completedForm.completed_form_fields[formField.id]
-                            }}
+                            {{ completedForm.completed_form_fields[formField.id] }}
                           </li>
                         </ul>
                       </div>
@@ -127,21 +120,8 @@
                       show-weeknumbers
                     >
                       <template v-slot="{ inputEvents, inputValue }">
-                        <div
-                          class="
-                            d-flex
-                            flex-col
-                            sm:flex-row
-                            justify-content-center
-                            text-center
-                            item-center
-                            btn-container-calendar
-                          "
-                        >
-                          <i
-                            for="last_update"
-                            class="fas fa-light fa-calendar p-2"
-                          ></i>
+                        <div class="d-flex flex-col sm:flex-row justify-content-center text-center item-center btn-container-calendar">
+                          <i for="last_update" class="fas fa-light fa-calendar p-2" aria-hidden="true"></i>
                           <input
                             id="last_update"
                             class="p-1 w-full"
@@ -158,17 +138,6 @@
                         </div>
                       </template>
                     </v-date-picker>
-                    <!-- <b-form-datepicker
-                    v-model="completedForm.last_update"
-                    :max="max"
-                    required
-                    id="last_update"
-                    class="mb-2"
-                    :disabled="isUpdateMode"
-                    locale="fr"
-                    @input="selectLastUpdate()"
-                  >
-                  </b-form-datepicker> -->
                   </b-form-group>
                 </b-row>
               </tab-content>
