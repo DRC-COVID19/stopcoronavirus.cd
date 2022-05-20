7819985f64f7d8e87d89ecedc69eed81caeaa967
rkota <rosaire@kinshasadigital.com>
2022-05-20 10:03:55 +0100

    refactor: update renderMapbox method in hspitals create page

--- resources/js/dashboard/pages/administrator/hospitals/components/Create.vue
+++ resources/js/dashboard/pages/administrator/hospitals/components/Create.vue
@@ -342,19 +342,7 @@ export default {
         // initialisation
         Mapbox.accessToken = this.MAPBOX_TOKEN
         const marker = new Mapbox.Marker()
-        const markerHeight = 50
-        const markerRadius = 10
-        const linearOffset = 25
-        const popupOffsets = {
-          top: [0, 0],
-          'top-left': [0, 0],
-          'top-right': [0, 0],
-          bottom: [0, -markerHeight],
-          'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
-          'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
-          left: [markerRadius, (markerHeight - markerRadius) * -1],
-          right: [-markerRadius, (markerHeight - markerRadius) * -1]
-        }
+        const popupOffsets = this.getPopupOffset()
         const popup = new Mapbox.Popup({ offset: popupOffsets, className: 'my-class' })
 
         const map = new Mapbox.Map({
@@ -399,6 +387,21 @@ export default {
         })
       } catch (error) {
         throw new Error(error)
+      }
+    },
+    getPopupOffset () {
+      const markerHeight = 50
+      const markerRadius = 10
+      const linearOffset = 25
+      return {
+        top: [0, 0],
+        'top-left': [0, 0],
+        'top-right': [0, 0],
+        bottom: [0, -markerHeight],
+        'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+        'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+        left: [markerRadius, (markerHeight - markerRadius) * -1],
+        right: [-markerRadius, (markerHeight - markerRadius) * -1]
       }
     }
   }
