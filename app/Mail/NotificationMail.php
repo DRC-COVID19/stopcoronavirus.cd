6204028c60aa2f15c2c61fb5304af136fc918289
gaelmap <gmapwata6@gmail.com>
2022-05-31 17:17:35 +0100

    fix:sending mail

--- app/Mail/NotificationMail.php
+++ app/Mail/NotificationMail.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class NotificationMail extends Mailable
+class NotificationMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
