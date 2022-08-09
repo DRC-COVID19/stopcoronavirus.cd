e03557beab0bd94b3c34acb7336c5c055251c218
rkota <rosairekota@gmail.com>
2022-08-09 16:53:30 +0100

    feat: add responsive

--- resources/js/dashboard/pages/dashboard/reporting/components/Pivottable.vue
+++ resources/js/dashboard/pages/dashboard/reporting/components/Pivottable.vue
@@ -70,7 +70,7 @@ export default {
 <style lang="scss" scoped>
 @import "@~/sass/_variables";
 .pivot-table-ui::v-deep{
-  @media screen and (max-width:576px) {
+  @media screen and (max-width:768px) {
      &{
       width: 100vw !important;
      }
@@ -85,6 +85,9 @@ export default {
       }
   }
   @media screen and (width: 768px) {
+     &{
+        width: 90vw !important;
+     }
       .pvtRenderers{
           width: 40% !important;
      }
